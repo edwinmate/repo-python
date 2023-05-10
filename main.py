4b7a76d9d8546b593fed2f6e53888bd3a521438a
edwinmate <mateomontoyaherrera@gmail.com>
2023-05-10 17:00:58 -0500

    use un for

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("Hola Mundo")
 my_lista=[20,30,80,80]
 
 my_dic={'saludo':'hola','despedida':'chao','animal':'gato'}
+
+for element in my_lista:
+    print(element)
+    if element == 20:
+        print(element)
+        break
